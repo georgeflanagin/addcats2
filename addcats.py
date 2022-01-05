a2eb8dd0e0dd6a7563880d2bd45fd430c9ac933e
George Flanagin <gflanagi@richmond.edu>
2022-01-05 14:17:29 -0500

    Added help.

--- addcats.py
+++ addcats.py
@@ -17,6 +17,8 @@ if sys.version_info < min_py:
 # Other standard distro imports
 ###
 import argparse
+import contextlib
+import textwrap
 
 ###
 # From hpclib
@@ -42,14 +44,58 @@ __status__ = 'in progress'
 __license__ = 'MIT'
 
 
+add_group            = lambda group : f"""sudo /usr/local/sbin/hpcgroupadd {group}"""
+add_user_to_group    = lambda user, group : f"""sudo /usr/local/sbin/hpcgpasswd -a {user} {group}"""
+drop_user_from_group = lambda user, group : f"""sudo /usr/local/sbin/hpcgpasswd -d {user} {group}"""
+manage               = lambda user : f"""sudo /usr/local/sbin/hpcmanage {user}"""
+
 def read_whitespace_file(filename:str) -> tuple:
-    with open(filename) as f:
-        data = f.read()
-    data = (" ".join(data.split('\n'))).split()
-    for _ in data:
+    """
+    This is a generator that returns the whitespace delimited tokens 
+    in a text file, one token at a time.
+    """
+
+    if not os.path.isfile(filename):
+        sys.stderr.write(f"{filename} cannot be found.")
+        return os.EX_NOINPUT
+
+    f = open(filename)
+    for _ in (" ".join(f.read().split('\n'))).split():
         yield _
     
-    
+
+addcats_help="""
+
+    Explanation:
+
+    The output file will default to stdout if it is not given. Otherwise,
+    the output of this program is written to whatever file you name. 
+
+    The input file should be a whitespace delimited file of netids. They
+    can be all on one line, or arbitrarily arranged by lines, including
+    blank lines. Each of these netids is automatically added to the student 
+    group and the managed group.
+
+    There must be a named faculty member who is sponsoring the netids for
+    which accounts are being created. The netids in the file will be associated
+    with this faculty member. The faculty member's account will be created
+    if it does not exist. All faculty are automatically added to the faculty
+    group and the managed group.
+
+    You can name one or more groups of additional association. These groups
+    will be created if they do not exist.
+
+    **NOTE** Repeatedly adding a user to a group does not create an error.
+
+    Example:
+
+    These two commands both write the result to newusers.sh:
+
+    python addcats.py -f smehkari -i saif.students -g econ -o newusers.sh
+    python addcats.py -f smehkari -i saif.students -g econ > newusers.sh
+
+    """
+
 
 @trap
 def addcats_main(myargs:argparse.Namespace) -> int:
@@ -67,11 +113,11 @@ def addcats_main(myargs:argparse.Namespace) -> int:
     if not linuxutils.group_exists(myargs.faculty):
         print(add_group(myargs.faculty))
         print(add_group(dollar_group))
+        print(manage(myargs.faculty))
+        print(add_user_to_group(myargs.faculty, myargs.faculty))
+        print(add_user_to_group(myargs.faculty, dollar_group))
+        print(add_user_to_group(myargs.faculty, 'faculty'))
 
-    print(manage(myargs.faculty))
-    print(add_user_to_group(myargs.faculty, myargs.faculty))
-    print(add_user_to_group(myargs.faculty, dollar_group))
-    print(add_user_to_group(myargs.faculty, 'faculty'))
     for g in myargs.group:
         if not linuxutils.group_exists(g):
             print(add_group(g))
@@ -89,26 +135,25 @@ def addcats_main(myargs:argparse.Namespace) -> int:
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(prog="addcats", 
-        description="What addcats does, addcats does best.")
+        description="What addcats does, addcats does best.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent(addcats_help))
 
     parser.add_argument('-f', '--faculty', type=str, required=True,
         help="Name of a faculty member.")
     parser.add_argument('-g', '--group', action='append', default=[],
-        help="Name of additional groups to add to the users.")
+        help="Name of additional groups to add to the users. Defaults to none.")
     parser.add_argument('-i', '--input', type=str, default="",
-        help="Input file name.")
+        help="Input file name with student netids.")
     parser.add_argument('-o', '--output', type=str, default="",
-        help="Output file name")
-    parser.add_argument('-v', '--verbose', action='store_true', 
-        help="Be chatty about what is taking place -- on a scale of 0 to 3")
-
+        help="Output file name; defaults to stdout.")
 
     myargs = parser.parse_args()
-    myargs.verbose and linuxutils.dump_cmdline(myargs)
 
     try:
-        vm_callable = "{}_main".format(os.path.basename(__file__)[:-3])
-        sys.exit(globals()[vm_callable](myargs))
+        outfile = sys.stdout if not myargs.output else open(myargs.output, 'w')
+        with contextlib.redirect_stdout(outfile):
+            sys.exit(globals()[f"{os.path.basename(__file__)[:-3]}_main"](myargs))
 
     except Exception as e:
         print(f"Unhandled exception {e}")
