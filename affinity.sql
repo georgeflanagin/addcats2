00c6a2d5514977164f28ebe9e603d5734e8f7420
George Flanagin <gflanagi@richmond.edu>
2022-12-02 10:30:45 -0500

    Added table for user aliases.

--- affinity.sql
+++ affinity.sql
@@ -1,16 +1,22 @@
-create table faculty_master (
+CREATE TABLE faculty_master (
     faculty varchar(20)
     );
+CREATE UNIQUE INDEX faculty_idx on faculty_master(faculty);
 
-create table faculty_partitions (
+CREATE TABLE faculty_partitions (
     faculty varchar(20),
     partition varchar(20),
     foreign key(faculty) references faculty_master(faculty)
     );
 
-create table faculty_student (
-    faculty varchar(20),
-    student varchar(20),
-    foreign key(faculty) references faculty_master(faculty)
+CREATE TABLE aliases (
+    netid varchar(20) primary key, 
+    alias_name varchar(20)
     );
 
+CREATE TABLE "faculty_student" (
+    faculty varchar(20), 
+    student varchar(20), 
+    when_added datetime default current_timestamp, 
+    foreign key(faculty) references faculty_master(faculty)
+    );
